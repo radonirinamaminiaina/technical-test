abee99e9ab081f3d6b47570f852378edcd595334
Rado <radonirina.maminiaina@gmail.com>
2023-01-31 11:28:22 +0300

    theme toggler

--- src/components/ThemeToggle/Component.tsx
+++ src/components/ThemeToggle/Component.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 import './styles.css';
 
 export default function Component() {
   const [checked, setChecked] = React.useState(true);
+
+  const { theme, switchTheme } = useContext(ThemeContext);
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log("Oops!, doesn't work, check /challenges/06-switch-theme to solve this issue");
+    switchTheme(theme === 'light' ? 'dark' : 'light')
     setChecked(e.target.checked);
   };
 
--- src/components/contexts/ThemeContext.tsx
+++ src/components/contexts/ThemeContext.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import { createContext, useContext, PropsWithChildren } from 'react';
+import { createContext, useContext, PropsWithChildren, useState } from 'react';
 
 export declare type ThemeValue = 'dark' | 'light';
 
 export interface ThemeContextValue<T extends string> {
   theme: T;
+  switchTheme(theme: string):void;
 }
 
 interface ThemeProviderProps<T> {
@@ -14,14 +15,22 @@ interface ThemeProviderProps<T> {
 
 export const ThemeContext = createContext<ThemeContextValue<ThemeValue>>({
   theme: 'light',
+  switchTheme:() => {}
 });
 
 export const ThemeProvider = ({ children, defaultTheme }: PropsWithChildren<ThemeProviderProps<ThemeValue>>) => {
+  const [currentTheme, setCurrentTheme] = useState<ThemeValue>('light')
+
+  const handleSwitchTheme = (theme: ThemeValue) => {
+    setCurrentTheme(theme)
+  }
+
   return (
     <ThemeContext.Provider
       value={{
         // @todo: handle this with a state, that can toggle between dark and light
-        theme: defaultTheme || 'light',
+        theme: currentTheme,
+        switchTheme: handleSwitchTheme
       }}
     >
       {children}
