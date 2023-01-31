019a9d264b4442870b5ba16f103ac27155999b64
Rado <radonirina.maminiaina@gmail.com>
2023-01-31 11:12:25 +0300

    stream.passThrough

--- src/lib/passthrough.ts
+++ src/lib/passthrough.ts
@@ -1,5 +1,8 @@
 import { ServerResponse } from 'http';
 
+const fs = require('fs');
+const stream = require('stream')
+
 export default function passthrough(filepath: string, res: ServerResponse): ServerResponse {
   // @todo:
   // 1. implement this function, use `PassThrough` stream to pipe the file content to the response explain what `PassThrough` stream is and why it is used here, could you tell another way to do this?
@@ -8,5 +11,13 @@ export default function passthrough(filepath: string, res: ServerResponse): Serv
   //   - X-Metadata: technical-test
   // to see result, check `http://localhost:3000/api/storages/working.json`
 
-  return res.end(`// @todo: implement this function, use \`PassThrough\` stream to show content of ${filepath}, then set additional headers`);
+  res.setHeader('Cache-Control', 'max-age=3600, public')
+  res.setHeader('X-Metadata', 'technical-test')
+  
+  // we use passThrough to pass byte value to the response in the browser
+  // Another way to achieve this is using fs.readFile and then write the result in response
+  const fileStream = new stream.PassThrough();
+  fs.createReadStream(filepath).pipe(fileStream);
+
+  return fileStream.pipe(res);
 }
