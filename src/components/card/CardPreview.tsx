04a2d09de1fe129788553fc1ca22044ff50a8cfd
Rado <radonirina.maminiaina@gmail.com>
2023-01-31 12:14:06 +0300

    card integration in progress

--- src/components/card/CardPreview.tsx
+++ src/components/card/CardPreview.tsx
@@ -1,7 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
 'use client';
 
+import Link from 'next/link';
 import { memo } from 'react';
+import CardBackIcon from './icons/CardBackIcon';
+import CardShopIcon from './icons/CardShopIcon';
 import './styles.css';
 
 /**
@@ -17,7 +20,33 @@ import './styles.css';
 function CardPreview() {
   return (
     <div className="preview-card">
-      <img alt="fake card" src="/images/card.png" />
+      <div className='preview-card__upper'>
+        <div className='preview-card__header horizontal-padding'>
+          <Link href="" className='preview-card__back'>
+            <CardBackIcon />
+          </Link>
+          <Link href="" className='preview-card__shop with-item'>
+            <CardShopIcon />
+          </Link>
+        </div>
+        <div className='preview-card__info horizontal-padding'>
+          <div className='preview-card__info-item'>
+            <h3>Platform</h3>
+            <p>PS5</p>
+          </div>
+          <div className='preview-card__info-item'>
+            <h3>Release</h3>
+            <p>Fall 2020</p>
+          </div>
+          <div className='preview-card__info-item'>
+            <h3>Price</h3>
+            <p>$50</p>
+          </div>
+        </div>
+        <figure className='preview-card__img'>
+          <img src="/images/ps5.png" />
+        </figure>
+      </div>
     </div>
   );
 }
